c329134c591b4a0f8978ddad81082b04692f0893
Victor <em3888@gmail.com>
2017-07-07 18:21:02 +0800

    add GoRoutine and Channels

---
+++ channels.go
@@ -0,0 +1,13 @@
+package main
+
+/**
+ * Channels are the pipes that connect concurrent goroutines. You can send values into channels from one goroutine and receive those values into another goroutine.
+
+ *
+ * @param  url  an absolute URL giving the base location of the image
+ * @param  name the location of the image, relative to the url argument
+ * @return      the image at the specified URL
+ * @see         Image
+ * @7/7/2017 6:20 PM
+ * Author : cyrsis@github 
+ */
